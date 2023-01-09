b437577a6a17d5aac2791eb6d0a7079952b9e191
afhjtdff <114405776+afhjtdff@users.noreply.github.com>
2023-01-09 19:56:40 +0300

    Update help.py

--- help.py
+++ help.py
@@ -9,8 +9,8 @@ soursce = '''
 `- -- -- -- -- -- -- -- --`
 **Sedthon source is working ⚡
 Dev : 𝙩𝙤𝙥𝙖𝙘
-Dev : @S_Y_V
-Source Ch : @N1111V**
+Dev : @s_4_g1
+Source Ch : @iraq11store**
 `-- -- -- -- -- -- -- -- --`
 '''
 trans = '''
